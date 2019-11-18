61063d676b1805d1746bab6f096619b428ae6bfc
Stephanie Samson <sasamson98@hotmail.com>
2019-11-18 13:00:15 -0500

    updated page with user data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // User Components
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
-import Profile  from "./components/user/Profile";
+import Profile from "./components/user/Profile";
 // Website Components
 import WebsiteList from "./components/website/WebsiteList";
 import WebsiteNew from "./components/website/WebsiteNew";
@@ -20,7 +20,6 @@ import WidgetChooser from "./components/widget/WidgetChooser";
 import WidgetEdit from "./components/widget/WidgetEdit";
 
 function App() {
-
   // user data
   const [users, setUsers] = useState([
     {
@@ -41,42 +40,96 @@ function App() {
     },
     {
       _id: "345",
-      username: "charly", 
-      password: "charly", 
-      firstName: "Charly", 
-      lastName: "Garcia", 
+      username: "charly",
+      password: "charly",
+      firstName: "Charly",
+      lastName: "Garcia",
       email: "charly@ulem.com"
     },
     {
-      _id: "456", 
-      username: "shiyu", 
-      password: "shiyu", 
-      firstName: "Shiyu", 
-      lastName: "Wang", 
+      _id: "456",
+      username: "shiyu",
+      password: "shiyu",
+      firstName: "Shiyu",
+      lastName: "Wang",
       email: "swang@ulem.org"
     }
-    ]);
+  ]);
 
-    function login(username, password) {
+  // Add a new user into users
+  const addUser = user => {
+    setUsers([...users, user]);
+  };
 
-    }
-    
+  // update user by id
+  const updateUser = newUser => {
+    setUsers(
+      users.map(user => {
+        if (user._id === newUser._id) {
+          return newUser;
+        } else {
+          return user;
+        }
+      })
+    );
+  };
+
   return (
     <Router>
       <Switch>
-      <Route exact path="/login" component={Login}></Route>
-      <Route exact path="/register" component={Register}></Route>
-      <Route exact path="/user/:uid" component={Profile}></Route>
-      <Route exact path="/user/:uid/website" component={WebsiteList}></Route>
-      <Route exact path="/user/:uid/website/new" component={WebsiteNew}></Route>
-      <Route exact path="/user/:uid/website/:wid" component={WebsiteEdit}></Route>
-      <Route exact path="/user/:uid/website/:wid/page" component={PageList}></Route>
-      <Route exact path="/user/:uid/website/:wid/page/new" component={PageNew}></Route>
-      <Route exact path="/user/:uid/website/:wid/page/:pid" component={PageEdit}></Route>
-      <Route exact path="/user/:uid/website/:wid/page/:pid/widget" component={WidgetList}></Route>
-      <Route exact path="/user/:uid/website/:wid/page/:pid/widget/new" component={WidgetChooser}></Route>
-      <Route exact path="/user/:uid/website/:wid/page/:pid/widget/:wgid" component={WidgetEdit}></Route>
-      <Route path="/" component={Login}></Route>
+        <Route exact path="/login">
+          <Login users={users} />
+        </Route>
+        <Route exact path="/register">
+          <Register users={users} addUser={addUser} />
+        </Route>
+        <Route exact path="/user/:uid">
+          <Profile users={users} updateUser={updateUser} />
+        </Route>
+        <Route exact path="/user/:uid/website" component={WebsiteList}></Route>
+        <Route
+          exact
+          path="/user/:uid/website/new"
+          component={WebsiteNew}
+        ></Route>
+        <Route
+          exact
+          path="/user/:uid/website/:wid"
+          component={WebsiteEdit}
+        ></Route>
+        <Route
+          exact
+          path="/user/:uid/website/:wid/page"
+          component={PageList}
+        ></Route>
+        <Route
+          exact
+          path="/user/:uid/website/:wid/page/new"
+          component={PageNew}
+        ></Route>
+        <Route
+          exact
+          path="/user/:uid/website/:wid/page/:pid"
+          component={PageEdit}
+        ></Route>
+        <Route
+          exact
+          path="/user/:uid/website/:wid/page/:pid/widget"
+          component={WidgetList}
+        ></Route>
+        <Route
+          exact
+          path="/user/:uid/website/:wid/page/:pid/widget/new"
+          component={WidgetChooser}
+        ></Route>
+        <Route
+          exact
+          path="/user/:uid/website/:wid/page/:pid/widget/:wgid"
+          component={WidgetEdit}
+        ></Route>
+        <Route path="/">
+          <Login users={users} />
+        </Route>
       </Switch>
     </Router>
   );
