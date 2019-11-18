bb09c1f331a5163dbfa0d7b5905da217a15b69c2
Stephanie Samson <sasamson98@hotmail.com>
2019-11-18 13:05:43 -0500

    updated user pages

--- client/src/components/user/Login.js
+++ client/src/components/user/Login.js
@@ -1,34 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+export default function Login(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default function Login() {
-    return (
-        <div class="container">
-        <h1>Login</h1>  
-        <form>
-            {/* <!--child of form - using bootstrap class  --> */}
-            <div className="form-group">
-                <label for="username">Username</label>
-                <input 
-                    type="text"
-                    id="username"
-                    placeholder="Username"
-                    className="form-control"
-                    />  
-            </div> 
-            <div className="form-group">
-                <label for="password">Password</label>
-                <input 
-                    type="password"
-                    id="password"
-                    placeholder="Password"
-                    className="form-control"
-                    />
-            </div>
-            {/* <!-- Buttons and links that look like buttons, must use bootstrap classes btn and btn-block classes to render them as buttons where appropriate. --> */}
-            <Link to="/user/:uid" className="btn btn-primary btn-block">Login</Link>
-            <Link to="/Register" className="btn btn-success btn-block">Register</Link>
-        </form>
+  let history = useHistory();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    for (let user of props.users) {
+      // We found the user
+      if (user.username === username && user.password === password) {
+        // /user/:uid
+        history.push(`/user/${user._id}`);
+        return;
+      }
+    }
+    // we can't find user
+    alert("Invalid credential, please try it again.");
+  }
+
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <form onSubmit={onSubmit}>
+        {/* <!--child of form - using bootstrap class  --> */}
+        <div className="form-group">
+          {/* <label for="username">Username</label> */}
+          <input
+            type="text"
+            // id="username"
+            placeholder="Username"
+            className="form-control"
+            value={username}
+            onChange={e => {
+              setUsername(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-group">
+          {/* <label for="password">Password</label> */}
+          <input
+            type="password"
+            // id="password"
+            placeholder="Password"
+            className="form-control"
+            value={password}
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        {/* <!-- Buttons and links that look like buttons, must use bootstrap classes btn and btn-block classes to render them as buttons where appropriate. --> */}
+
+        <button className="btn btn-primary btn-block">Login</button>
+        <Link to="/Register" className="btn btn-success btn-block">
+          Register
+        </Link>
+      </form>
     </div>
-    )
-};
+  );
+}
