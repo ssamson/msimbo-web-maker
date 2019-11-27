38b175237f2e597cbf0e1737b97ab7a05ce62b6b
Stephanie Samson <sasamson98@hotmail.com>
2019-11-27 11:53:51 -0500

    send hello to client on user page

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 // send hello to client
 router.get("/", (req, res) => {
-  res.json("Hello");
+  res.json("Hello from server");
+});
+
+router.get("/123", (req, res) => {
+  res.json("hello from 123");
 });
 
 module.exports = router;
