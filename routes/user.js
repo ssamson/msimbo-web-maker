a206bb287c0d0e3d81a82777e39025f92fbe940b
Stephanie Samson <sasamson98@hotmail.com>
2019-12-02 11:23:22 -0500

    moved users to server side

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,107 @@
 const express = require("express");
 const router = express.Router();
 
-// send hello to client
+const users = [
+  {
+    _id: "123",
+    username: "alice",
+    password: "alice",
+    firstName: "Alice",
+    lastName: "Wonder",
+    email: "alice@gmail.com"
+  },
+  {
+    _id: "234",
+    username: "bob",
+    password: "bob",
+    firstName: "Bob",
+    lastName: "Marley",
+    email: "bob@whatever.com"
+  },
+  {
+    _id: "345",
+    username: "charly",
+    password: "charly",
+    firstName: "Charly",
+    lastName: "Garcia",
+    email: "charly@ulem.com"
+  },
+  {
+    _id: "456",
+    username: "shiyu",
+    password: "shiyu",
+    firstName: "Shiyu",
+    lastName: "Wang",
+    email: "swang@ulem.org"
+  }
+];
+
+// Find user by credentials
 router.get("/", (req, res) => {
-  res.json("Hello from server");
+  // get username and password that user enter
+  const username = req.query.username;
+  const password = req.query.password;
+  let user;
+  // if username and password are sent from client
+  if (username && password) {
+    for (let i = 0; i < users.length; i++) {
+      // if we found a user with given username and password
+      if (users[i].username === username && users[i].password === password) {
+        user = users[i];
+      }
+    }
+    // if the username is taken
+  } else if (username) {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username === username) {
+        user = users[i];
+      }
+    }
+  }
+  // if user is not existing
+  if (!user) {
+    user = null;
+  }
+  // send user back to client
+  res.json(user);
 });
 
-router.get("/123", (req, res) => {
-  res.json("hello from 123");
+// Create new user
+router.post("/", (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.json(newUser);
 });
+
+// Find user by id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  let user = null;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i]._id === id) {
+      user = users[i];
+    }
+  }
+  res.json(user);
+});
+
+// Update user
+router.put("/", (req, res) => {
+  const newUser = req.body;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i]._id === newUser._id) {
+      users[i] = newUser;
+    }
+  }
+  res.json(newUser);
+});
+// send hello to client
+// router.get("/", (req, res) => {
+//   res.json("Hello from server");
+// });
+
+// router.get("/123", (req, res) => {
+//   res.json("hello from 123");
+// });
 
 module.exports = router;
