c5c5ca4398302dd74e615b6ac04704c7603a3c5f
Stephanie Samson <sasamson98@hotmail.com>
2019-12-12 15:10:22 -0500

    mongodb, user

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const db = config.get("mongo");
+
+async function connectDB() {
+  await mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+}
+module.exports = connectDB;
