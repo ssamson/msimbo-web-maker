093afb42cc50f608b367bf0c8b588d4b6dc12fa1
Stephanie Samson <sasamson98@hotmail.com>
2019-12-13 09:49:56 -0500

    mongodb for user

--- routes/user.js
+++ routes/user.js
@@ -3,40 +3,40 @@ const router = express.Router();
 
 const User = require("../models/User");
 
-const users = [
-  {
-    _id: "123",
-    username: "alice",
-    password: "alice",
-    firstName: "Alice",
-    lastName: "Wonder",
-    email: "alice@gmail.com"
-  },
-  {
-    _id: "234",
-    username: "bob",
-    password: "bob",
-    firstName: "Bob",
-    lastName: "Marley",
-    email: "bob@whatever.com"
-  },
-  {
-    _id: "345",
-    username: "charly",
-    password: "charly",
-    firstName: "Charly",
-    lastName: "Garcia",
-    email: "charly@ulem.com"
-  },
-  {
-    _id: "456",
-    username: "shiyu",
-    password: "shiyu",
-    firstName: "Shiyu",
-    lastName: "Wang",
-    email: "swang@ulem.org"
-  }
-];
+// const users = [
+//   {
+//     _id: "123",
+//     username: "alice",
+//     password: "alice",
+//     firstName: "Alice",
+//     lastName: "Wonder",
+//     email: "alice@gmail.com"
+//   },
+//   {
+//     _id: "234",
+//     username: "bob",
+//     password: "bob",
+//     firstName: "Bob",
+//     lastName: "Marley",
+//     email: "bob@whatever.com"
+//   },
+//   {
+//     _id: "345",
+//     username: "charly",
+//     password: "charly",
+//     firstName: "Charly",
+//     lastName: "Garcia",
+//     email: "charly@ulem.com"
+//   },
+//   {
+//     _id: "456",
+//     username: "shiyu",
+//     password: "shiyu",
+//     firstName: "Shiyu",
+//     lastName: "Wang",
+//     email: "swang@ulem.org"
+//   }
+// ];
 
 // Find user by credentials
 router.get("/", async (req, res) => {
